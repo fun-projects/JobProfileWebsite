700fbaf925f18015d8b2c87bd08515635ffda16c
lakshmanvvs <lakshmanvvs@gmail.com>
2015-06-17 20:37:56 -0500

    Update aboutRoute.js
    Added code for AboutRoute.js 
    Please review

--- server/routes/aboutRoute.js
+++ server/routes/aboutRoute.js
@@ -1,1 +1,50 @@
 var User = require('../models/AboutModel');
+//
+var About = require('../models/AboutModel');
+
+var About = function (route){
+	route
+	.use('/About/:id',function(req,res,nxt){
+		if(req.params.id== null){
+	   			res.status(500).send({message:'Invalid request'});
+	   			nxt();
+	    
+	       }
+	 		nxt();
+     })
+     .get('/About',function(req,res){
+        About.find({},function(err,data){
+         if(err)
+            res.status(500).send(err);
+         else
+            res.status(200).send(data);
+        });
+     })
+      .post('/About',function(req,res){
+      	var About = new About(req.body);
+      	About.save(function(err,data){
+      		if(err)
+				res.status(500).send(err);
+			else
+				res.status(200).send(data);
+      	});
+      })
+      .put('/About/:id',function(req,res){
+		About.update({_id:req.params.id},req.body,function(err,data){
+			if(err)
+				res.status(500).send(err);
+			else
+				res.status(200).send(data);
+		});
+	 })
+      .delete('/About/:id',function(req,res){
+      	About.remove({_id:req.params.id},function(err,data){
+      		if(err)
+      			res.status(500).send(err);
+      		else
+      			res.status(200).send(data);
+      	});
+      });
+ }
+
+ module.exports = About;
