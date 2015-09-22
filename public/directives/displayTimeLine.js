93ad0c0f7a2b2bb1bf66ad3c9d46b181fc66755c
Lakshman <lakshmanvvs@gmail.com>
2015-09-22 11:05:21 -0500

    Update displayTimeLine.js
    changed code to include scope: true

--- public/directives/displayTimeLine.js
+++ public/directives/displayTimeLine.js
@@ -3,8 +3,10 @@ angular.module('JobApp').directive("displayTimeLine", function(){
 		restrict: "EAC",
 		templateUrl: "templates/timeLine.html",
 		transclude: true,
+		scope: true,
 		controller:function($scope,$location,$state){
 			$scope.clickTimeLine = function(){
+				conosle.log("inside clicktimeline funcion");
 				switch($scope.timeline.type){
 					case "project":
 						//$location.path("/projects/view/"+$scope.timeline._id);
