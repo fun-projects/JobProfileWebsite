79184b0f2467fddc6e0f9f519e3e77082259e465
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-09-22 19:39:01 -0500

    fixed err

--- public/directives/displayTimeLine.js
+++ public/directives/displayTimeLine.js
@@ -6,7 +6,6 @@ angular.module('JobApp').directive("displayTimeLine", function(){
 		scope: true,
 		controller:function($scope,$location,$state){
 			$scope.clickTimeLine = function(){
-				conosle.log("inside clicktimeline funcion");
 				switch($scope.timeline.type){
 					case "project":
 						//$location.path("/projects/view/"+$scope.timeline._id);
